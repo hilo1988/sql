829e2cb2d4a7abc1e31b30e215a484b47e9e861b
Takehiro Nakamori <tnakamori@whizz-tech.co.jp>
2013-10-22 15:12:15 +0900

    Serializable追加

--- sql/src/main/java/com/yoidukigembu/sql/entity/AbstractBaseEntity.java
+++ sql/src/main/java/com/yoidukigembu/sql/entity/AbstractBaseEntity.java
@@ -1,7 +1,17 @@
 package com.yoidukigembu.sql.entity;
 
+import java.io.Serializable;
 
-public abstract class AbstractBaseEntity {
+
+/**
+ * エンティティの基底クラス。
+ * DAOを使用するエンティティはこのクラスを継承してください。
+ * @author Takehiro Nakamori
+ *
+ */
+public abstract class AbstractBaseEntity implements Serializable {
+
+	private static final long serialVersionUID = -2326129671254188506L;
 
 
 }
