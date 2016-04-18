e91fa18bd071ef6c83da86a10cd3c9763ff1b110
hilo <yoidukigembu@gmail.com>
2016-04-19 00:16:45 +0900

    テストソースリファクタ

--- sql/sql/src/test/java/com/yoidukigembu/sql/selct/SelectTest.java
+++ sql/sql/src/test/java/com/yoidukigembu/sql/selct/SelectTest.java
@@ -16,22 +16,20 @@ public class SelectTest extends TestCase {
 
 	@Test
 	public void エンティティクラスのみのテスト() {
-		Select<DummyEntity> select = Select.from(DummyEntity.class);
-
-		select.generate((sql, params) -> {
-			assertEquals("SELECT T.* FROM dummy T", sql);
-			assertEquals(0, params.size());
-		});
+		Select.from(DummyEntity.class)
+			.generate((sql, params) -> {
+				assertEquals("SELECT T.* FROM dummy T", sql);
+				assertEquals(0, params.size());
+			});
 	}
 
 	@Test
 	public void エンティティクラスとエイリアスのテスト() {
-		Select<DummyEntity> select = Select.from(DummyEntity.class, "alias");
-
-		select.generate((sql, params) -> {
-			assertEquals("SELECT alias.* FROM dummy alias", sql);
-			assertEquals(0, params.size());
-		});
+		Select.from(DummyEntity.class, "alias")
+			.generate((sql, params) -> {
+				assertEquals("SELECT alias.* FROM dummy alias", sql);
+				assertEquals(0, params.size());
+			});
 	}
 
 	@Test
