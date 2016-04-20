599c4614438a7d850ae19fc589bec52ff5840bbe
hilo <yoidukigembu@gmail.com>
2016-04-21 00:43:36 +0900

    WhereBuilderをやめてWhereImplに変更

--- sql/sql/src/main/java/com/yoidukigembu/sql/where/impl/WhereImpl.java
+++ sql/sql/src/main/java/com/yoidukigembu/sql/where/impl/WhereImpl.java
@@ -1,28 +1,34 @@
-package com.yoidukigembu.sql.where;
+package com.yoidukigembu.sql.where.impl;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-public class WhereBuilder implements Where {
-	
+import com.yoidukigembu.sql.where.Where;
+
+/**
+ * Whereの実装クラス
+ * 
+ * @author hilo
+ *
+ */
+public class WhereImpl implements Where {
+
+	/** 条件ホルダリスト */
 	private final List<Where.WhereHolder> holderList;
-	
+
+	/** パラメータ */
 	private List<Object> params;
-	
+
+	/** クエリ */
 	private String query;
-	
+
 	private boolean builtFlg;
-	
-	private WhereBuilder() {
+
+	public WhereImpl() {
 		this.holderList = new ArrayList<>();
 		this.builtFlg = false;
-	}
-	
-	public static WhereBuilder newInstance() {
-		WhereBuilder builder = new WhereBuilder();
-		return builder;
 	}
 
 	@Override
@@ -38,20 +44,17 @@ public class WhereBuilder implements Where {
 		}
 		StringBuilder where = new StringBuilder();
 		params = new ArrayList<>();
-		
+
 		getHolderList().forEach(holder -> {
 			if (builtFlg) {
-				where.append(holder.getDelimiter().getValue())
-						.append(" ");
+				where.append(holder.getDelimiter().getValue()).append(" ");
 			}
-			where.append(holder.getQuery())
-				.append(" ");
-			
+			where.append(holder.getQuery()).append(" ");
+
 			if (!holder.isBlankFlg()) {
-				Object param = holder.getParam()
-						.orElseThrow(() -> new NullPointerException(
-								String.format("param is NULL. query：[%s]", holder.getQuery())));
-				
+				Object param = holder.getParam().orElseThrow(
+						() -> new NullPointerException(String.format("param is NULL. query：[%s]", holder.getQuery())));
+
 				if (param instanceof Collection) {
 					params.addAll((Collection<?>) param);
 				} else {
@@ -59,20 +62,17 @@ public class WhereBuilder implements Where {
 				}
 			}
 		});
-		
+
 		query = where.toString();
 		consumer.consume(getWhereQuery(), getParams());
 	}
-	
-	
+
 	private List<Object> getParams() {
-		return Optional.ofNullable(params)
-					.orElse(new ArrayList<>(0));
+		return Optional.ofNullable(params).orElse(new ArrayList<>(0));
 	}
-	
+
 	private String getWhereQuery() {
 		return Optional.ofNullable(query).orElse("");
 	}
-	
 
 }
